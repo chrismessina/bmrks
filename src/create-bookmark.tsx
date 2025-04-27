4836dc1e344585bff939156890892377af4b937f
Chris Messina <chris.messina@gmail.com>
2025-04-26 19:51:04 -0700

    Add auth logging and simplify groups data structure

--- src/create-bookmark.tsx
+++ src/create-bookmark.tsx
@@ -287,8 +287,25 @@ function CreateBookmark({ user }: { user: User }) {
         </ActionPanel>
       }
     >
-      <Form.Dropdown title="Group" placeholder="Select a group for your bookmark" {...itemProps.groupId}>
-        {groups && groups.map((group) => <Form.Dropdown.Item key={group.id} value={group.id} title={group.name} />)}
+      <Form.Dropdown 
+        title="Group" 
+        placeholder="Select a group for your bookmark" 
+        {...itemProps.groupId}
+      >
+        {groups && groups.length > 0 ? (
+          groups.map((group) => (
+            <Form.Dropdown.Item 
+              key={group.id} 
+              value={group.id} 
+              title={group.name || `Group ${group.id}`} 
+            />
+          ))
+        ) : (
+          <Form.Dropdown.Item 
+            value="all" 
+            title="Bookmarks" 
+          />
+        )}
       </Form.Dropdown>
       <Form.Separator />
       <Form.TextField
